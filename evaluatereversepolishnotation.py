36f46afac667606e5e04f40c6783539fa253e2eb
Melvin Dsouza <melvindsouza2003@gmail.com>
2026-02-22 14:59:11 +0530

    evaluatereversepolishnotation

---
+++ evaluatereversepolishnotation.py
@@ -0,0 +1,25 @@
+def evalRPN(self, tokens):
+
+        stack = []
+
+        for token in tokens:
+
+            if token in ["+", "-", "*", "/"]:
+
+                b = stack.pop()
+                a = stack.pop()
+
+                if token == "+":
+                    stack.append(a + b)
+                elif token == "-":
+                    stack.append(a - b)
+                elif token == "*":
+                    stack.append(a * b)
+                else:
+                    # division truncates toward zero
+                    stack.append(int(a / b))
+
+            else:
+                stack.append(int(token))
+
+        return stack[0]
