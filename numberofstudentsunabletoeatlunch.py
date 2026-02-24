fceb3824dd488b7c61fd60b5fc586cfeb37b0066
Melvin Dsouza <melvindsouza2003@gmail.com>
2026-02-24 20:55:47 +0530

    Numberofstudentsunabletoeatlunch

---
+++ numberofstudentsunabletoeatlunch.py
@@ -0,0 +1,17 @@
+ def countStudents(self, students, sandwiches):
+
+        count0 = students.count(0)
+        count1 = students.count(1)
+
+        for sandwich in sandwiches:
+
+            if sandwich == 0:
+                if count0 == 0:
+                    break
+                count0 -= 1
+            else:
+                if count1 == 0:
+                    break
+                count1 -= 1
+
+        return count0 + count1
