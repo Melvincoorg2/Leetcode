c8eeac69490e835486748164f3a1ea67ec863c97
Melvin Dsouza <melvindsouza2003@gmail.com>
2026-03-13 14:11:27 +0530

    allpathssourcetotarget

---
+++ allpathsfromsourcetotarget.py
@@ -0,0 +1,17 @@
+class Solution:
+ def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+    n = len(graph)
+    result = []
+
+    def dfs(node, path):
+        if node == n - 1:
+            result.append(path[:])
+            return
+        for nei in graph[node]:
+            path.append(nei)
+            dfs(nei, path)
+            path.pop()
+
+    dfs(0, [0])
+    return result
