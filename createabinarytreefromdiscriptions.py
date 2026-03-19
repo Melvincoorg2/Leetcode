2e8944ae9de3fe28618a2ee57314b8f5f9ca7218
Melvin Dsouza <melvindsouza2003@gmail.com>
2026-03-19 11:54:55 +0530

    createbinarytree

---
+++ createabinarytreefromdiscriptions.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+ def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+
+    nodes = {}
+    has_parent = set()
+
+    for parent, child, is_left in descriptions:
+        if parent not in nodes:
+            nodes[parent] = TreeNode(parent)
+        if child not in nodes:
+            nodes[child] = TreeNode(child)
+
+        if is_left:
+            nodes[parent].left = nodes[child]
+        else:
+            nodes[parent].right = nodes[child]
+
+        has_parent.add(child)
+
+    for val in nodes:
+        if val not in has_parent:
+            return nodes[val]
